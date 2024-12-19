73c7e3fe7740a32006b5559bc5032bff13510e3a
GrantBirki <grant.birkinbine@gmail.com>
2024-12-19 12:45:55 -0800

    add the admins

--- terraform/repo-2.tf
+++ terraform/repo-2.tf
@@ -3,3 +3,9 @@ resource "github_team_repository" "repo-2" {
   repository = "repo-2"
   permission = "push"
 }
+
+resource "github_team_repository" "repo-2" {
+  team_id    = "admins"
+  repository = "repo-2"
+  permission = "admin"
+}
